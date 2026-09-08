2768a2c7fc063347940864e3acfb40b9ea2f0833
Shay <shaysaha64@icloud.com>
2026-09-08 10:22:31 +0100

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        sl = list(s)
+        lis = list()
+        for c in sl:
+            if c.isalnum():
+                lis.append(c.lower()) 
+        m = list(lis)  
+        lis.reverse()
+        if m == lis:
+            return True
+
+        return False
+
+        #brute force solution but takes too much space form preprocessing string, removing unalphanum, converting to lowercase etc
